6dd4d4f61a6098f0896a798132cfe41f26feaf49
Manan Upadhyay <mananupadhyay2000@gmail.com>
2025-05-04 18:57:59 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="font-sans bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-white">
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
