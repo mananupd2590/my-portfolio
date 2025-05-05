b64766595cfc7fd244e9e1faeb854a8e1bde1b5d
Manan Upadhyay <mananupadhyay2000@gmail.com>
2025-05-04 18:18:38 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,22 +10,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 
-// src/App.jsx
-import React from 'react';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-
-function App() {
-  return (
-    <div className="font-sans bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-white">
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
-    </div>
-  );
-}
-
-export default App;
